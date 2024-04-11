dd945a4cffcee9d9fa7231298b4c4ee3fc1d9669
Alex Peterson <apeter18@terpmail.umd.edu>
2024-04-11 17:12:37 -0400

    added a complete export method

--- tracker.py
+++ tracker.py
@@ -121,8 +121,15 @@ class Tracker():
         """
         
     def export_data(self, filepath):
-        with open(filepath, "w", encoding = "utf-8"):
-            pass
+        """ 
+    A method that exports the str method to a textfile.
+
+    Args:
+        filepath: A file for the str method to write too.
+    """
+    with open(filepath, "w", encoding = "utf-8") as f:
+        f.write(str(self))
+            
         
 def display_summary(tracker):
     '''
