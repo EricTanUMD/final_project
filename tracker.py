f2eed6446e3b9bff1e079604d1dc867522fb6372
Eric Tan <etan123@terpmail.umd.edu>
2024-05-01 10:27:43 -0400

    simplified _str_

--- tracker.py
+++ tracker.py
@@ -52,6 +52,10 @@ class Tracker():
                     '''
                 # Parse through the file and insert workout information into the
                 # designated day
+                
+                days = {"Mo": 0, "Tu": 1, "We": 2, "Th": 3, "Fr": 4, 
+                           "Sa": 5, "Su": 6}
+                
                 with open(path, "r") as file:
                     for line in file:
                         match = re.match(regex, line)
@@ -59,55 +63,15 @@ class Tracker():
                         if (match is None):
                             raise ValueError("Wrong format for exercise")
                         else: # check which day each exercise is in
-                            if (match.group("day") == "Mo"):
-                                self.week[0].append( {
+                            day_index = days.get(match.group("day"))
+                            if day_index is not None:
+                                self.week[day_index].append({
                                     "muscle_group": match.group("muscle_group"),
                                     "workout": match.group("workout_type"),
                                     "time": match.group("time"),
                                     "reps": match.group("reps")
                                 })
-                            elif (match.group("day") == "Tu"):
-                                self.week[1].append ({
-                                    "muscle_group": match.group("muscle_group"),
-                                    "workout": match.group("workout_type"),
-                                    "time": match.group("time"),
-                                    "reps": match.group("reps")
-                                })
-                            elif (match.group("day") == "We"):
-                                self.week[2].append({
-                                    "muscle_group": match.group("muscle_group"),
-                                    "workout": match.group("workout_type"),
-                                    "time": match.group("time"),
-                                    "reps": match.group("reps")
-                                })
-                            elif (match.group("day") == "Th"):
-                                self.week[3].append({
-                                    "muscle_group": match.group("muscle_group"),
-                                    "workout": match.group("workout_type"),
-                                    "time": match.group("time"),
-                                    "reps": match.group("reps")
-                                })
-                            elif (match.group("day") == "Fr"):
-                                self.week[4].append({
-                                    "muscle_group": match.group("muscle_group"),
-                                    "workout": match.group("workout_type"),
-                                    "time": match.group("time"),
-                                    "reps": match.group("reps")
-                                })
-                            elif (match.group("day") == "Sa"):
-                                self.week[5].append({
-                                    "muscle_group": match.group("muscle_group"),
-                                    "workout": match.group("workout_type"),
-                                    "time": match.group("time"),
-                                    "reps": match.group("reps")
-                                })
-                            elif (match.group("day") == "Su"):
-                                self.week[6].append({
-                                    "muscle_group": match.group("muscle_group"),
-                                    "workout": match.group("workout_type"),
-                                    "time": match.group("time"),
-                                    "reps": match.group("reps")
-                                })
+            
                 
     # Made by Eric Tan
     def __str__(self):
